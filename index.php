74944a63dbc9ac99acd4a56c5d4c28781c4e8c73
calonso-conabio <calonso@conabio.gob.mx>
2015-10-30 17:05:11 -0600

    comentando lineas para debbug en ambientes de produccion

--- index.php
+++ index.php
@@ -22,8 +22,8 @@ $config = dirname(__FILE__) . '/protected/config/main.php';
 $appClass = dirname(__FILE__) . '/protected/components/WebApplication.php';
 
 // Disable these 3 lines when in production mode
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 5);
+//defined('YII_DEBUG') or define('YII_DEBUG', true);
+//defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 5);
 ini_set('error_reporting', E_ALL);
 
 require_once($yii);
